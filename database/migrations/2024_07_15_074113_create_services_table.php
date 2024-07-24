e8626568731de0101d5d5aaa3f1d1ff56109e3b5
Bagus Muhammad Wijaksono <106671990+baguswijaksono@users.noreply.github.com>
2024-07-24 17:02:55 +0700

    fix: create_services_table

--- database/migrations/2024_07_15_074113_create_services_table.php
+++ database/migrations/2024_07_15_074113_create_services_table.php
@@ -13,9 +13,8 @@ return new class extends Migration {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description')->nullable();
+            $table->text('description');
             $table->integer('price');
-            $table->integer('duration');
             $table->timestamps();
         });
     }
